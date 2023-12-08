d10850f223098f733d7585f89908a8d038f2e3b5
PoornaSyntax <poornasairam09@gmail.com>
2023-12-08 15:01:48 -0500

    my commit

--- src/main/java/project/MaxMin_B.java
+++ src/main/java/project/MaxMin_B.java
@@ -1,6 +1,6 @@
 package project;
 
-public class MaxMinBikal {
+public class MaxMin_B {
     public static void main(String[] args) {
         int[] num = {23,44,12,21,45,76,87,43,52,101};
         int min = num[0];
@@ -12,6 +12,7 @@ public class MaxMinBikal {
                 max = num[i];
             }
         }
+        //Print the Max and Min
         System.out.println("The minimum number in array is " +min);
         System.out.println("The maximum number in the array is " +max);
     }
